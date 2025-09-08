85efcc271d7c6d48d94bc79a1d479021e97f04dd
unknown <tokiosman96@gmail.com>
2025-09-08 16:10:26 +0600

    feat(layout):add header,sidebar and movielist component and created utilty function for getting movie cover image

--- cine-rental/src/App.jsx
+++ cine-rental/src/App.jsx
@@ -1,10 +1,19 @@
+import MovieList from "./cine/MovieList"
+import Header from "./Header"
+import Sidebar from "./Sidebar"
 
 function App() {
   
 
   return (
     <>
-     <h1 className="text-3xl text-red-300" >Hello Text</h1> 
+     <Header/>
+     <main>
+      <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <Sidebar/>
+        <MovieList/>
+      </div>
+     </main>
     </>
   )
 }
